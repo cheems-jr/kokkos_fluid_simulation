3b1694d908f7e00ac1a87353bd26ccb96afdcdf9
Rex Kendall <kendallr@ethz.ch>
2025-06-14 17:26:27 +0100

    karman vortex sim init

---
+++ karm_sim/include/sim.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <KokkosCore.hpp>
+
+class sim
+{
+private:
+    int xn_, yn_;
+    double dx_, dy_;
+    Kokkos::View<double**> u_, v_, p_;
+
+    void advect(double dt);
+    void solve_pressure(double dt);
+    void set_boundary_conditions();
+public:
+    sim(int xn, int yn, double xL, double yL);
+    void run(int steps);
+    void save_vtk(const std::string& filename);
+};
---
+++ karm_sim/src/main.cpp
@@ -0,0 +1,16 @@
+#include "sim.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    Kokkos::initialize(argc, argv);
+    {
+        const int xn =  256, yn = 128;
+        const double xL = 2.0, yL = 1.0;
+
+        sim sim(xn, yn, xL, yL);
+        sim.run(10000)
+        sim.save_vtk('final');
+    }
+    Kokkos::finalize();
+    return 0;
+}
