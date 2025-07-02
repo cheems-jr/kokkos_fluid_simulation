ab8ead02de0f0d3024245658acece25b2cbe5fea
Rex Kendall <kendallr@ethz.ch>
2025-07-02 19:24:08 +0100

    new method

--- karm_sim/include/sim.hpp
+++ karm_sim/include/sim.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Kokkos_Core.hpp>
+#include <cmath>
 
 class sim
 {
@@ -8,39 +9,51 @@ private:
     int xn_, yn_, T_;
     double dx_, dy_;
     Kokkos::View<double**> u_, v_, p_;
+    
+    static constexpr int i_1 = 96;
+    static constexpr int i_2 = 106;
+    static constexpr int j_1 = 96;
+    static constexpr int j_2 = 106;
+    static constexpr double dx = 1.0 / (i_2 - i_1);
+    static constexpr double dy = 1.0 / (j_2 - j_1);
+    static constexpr double Re = 70.0;
+    static constexpr double cfl = 0.2; 
+    static constexpr double omegap = 1.0;
+    static constexpr int maxitp = 100;
+    static constexpr double errorp = 0.0001;
+    // Use ternary operator for constexpr min
+    static constexpr double dt = cfl * (dx < dy ? dx : dy);
 
     void set_boundary_conditions_uv();
     void set_boundary_conditions_p();
     void set_initial_conditions();
     void velocity();
     void poisson_solver();
-    void save_vtk(const std::string& filename);
+    
 
     KOKKOS_INLINE_FUNCTION
-    bool in_square(int i, int j){
-        if (i_1 < i && i < i_2 && j_1 < j && j < j_2){
-            return true;
-        }
-        return false;
+    bool in_square(int i, int j) const {
+        return (i_1 < i && i < i_2 && j_1 < j && j < j_2);
     }
     KOKKOS_INLINE_FUNCTION
     double KKSchemeX(const double u, const Kokkos::View<double**> &f, const int i,
-                        const int j) {
-    return (u * (-f(i + 2,j) + 8.0 * (f(i + 1,j) - f(i - 1,j)) + f(i - 2,j)) / (12.0 * dx) +
-            abs(u) * (f(i + 2,j) - 4.0 * (f(i + 1,j) + f(i - 1,j)) +
-            6.0 * f(i,j) + f(i - 2,j)) / (4.0 * dx));
+                        const int j) const {
+        return (u * (-f(i + 2,j) + 8.0 * (f(i + 1,j) - f(i - 1,j)) + f(i - 2,j)) / (12.0 * dx) +
+                std::abs(u) * (f(i + 2,j) - 4.0 * (f(i + 1,j) + f(i - 1,j)) +
+                6.0 * f(i,j) + f(i - 2,j)) / (4.0 * dx));
     }
     KOKKOS_INLINE_FUNCTION
     double KKSchemeY(const double v, const Kokkos::View<double**> &f, const int i,
-                            const int j) {
-    return (v * (-f(i,j + 2) + 8.0 * (f(i,j + 1) - f(i,j - 1)) + f(i,j - 2)) / (12.0 * dy) +
-            abs(v) * (f(i,j + 2) - 4.0 * (f(i,j + 1) + f(i,j - 1)) +
-            6.0 * f(i,j) + f(i,j - 2)) / (4.0 * dy));
+                            const int j) const {
+        return (v * (-f(i,j + 2) + 8.0 * (f(i,j + 1) - f(i,j - 1)) + f(i,j - 2)) / (12.0 * dy) +
+                std::abs(v) * (f(i,j + 2) - 4.0 * (f(i,j + 1) + f(i,j - 1)) +
+                6.0 * f(i,j) + f(i,j - 2)) / (4.0 * dy));
     }
 
 public:
     sim(int xn, int yn);
     void run(double steps);
     void save_vtk(const std::string& filename);
+    void save_pressure_matrix_csv(const std::string& filename);
 };
 
--- karm_sim/src/sim.cpp
+++ karm_sim/src/sim.cpp
@@ -4,60 +4,43 @@
 #include <fstream>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 #include <algorithm>
 
-
-namespace karman_vortex {
-
-constexpr double Re = 70.0;  // Reynolds Number
-constexpr double cfl = 0.2;  // CFL Number
-
-/*SOR Pamameters*/
-constexpr double omegap = 1.0;
-constexpr int maxitp = 100;
-constexpr double errorp = 0.0001;
-
-/* set x-grid parameters*/
-constexpr int i_1 = 96;
-constexpr int i_2 = 106;
-
-/* set y-grid parameters*/
-constexpr int j_1 = 96;
-constexpr int j_2 = 106;
-
-/* set delta x,y,t*/
-constexpr double dx = 1.0 / (i_2 - i_1);
-constexpr double dy = 1.0 / (j_2 - j_1);
-constexpr double dt = cfl * fmin(dx, dy);
-
 sim::sim(int xn, int yn)
-  : xn_(xn), yn_(yn),
+  : xn_(xn), yn_(yn), dx_(1.0 / (i_2 - i_1)), dy_(1.0 / (j_2 - j_1)),
     u_("u", xn+2, yn+2), v_("v", xn+2, yn+2), p_("p", xn+2, yn+2)
 {
     Kokkos::deep_copy(u_, 1.0);
     Kokkos::deep_copy(v_, 0.0);
     Kokkos::deep_copy(p_, 0.0);
-    sim::set_boundary_conditions_uv();
-    sim::set_boundary_conditions_p();
+    Kokkos::fence();
+    set_boundary_conditions_uv();
+    Kokkos::fence();
+    set_boundary_conditions_p();
+    Kokkos::fence();
 }
 
 void sim::run(double steps) {    
     
     for (int step = 0; step < steps; step++) {
-        
-        sim::poisson_solver();
-        sim::velocity();
-        sim::set_boundary_conditions_uv();
+        poisson_solver();
+        Kokkos::fence();
+        velocity();
+        Kokkos::fence();
+        set_boundary_conditions_uv();
+        Kokkos::fence();
+        std::cout << "step: " << step << std::endl;
+        std::ostringstream ss;
+        ss << "../data/" << std::setw(6) << std::setfill('0') << step << ".csv";
+        save_pressure_matrix_csv(ss.str());
 
-        if (step % 5 == 0) {
-            save_vtk("output_step_" + std::to_string(step));
-        }
     }
 }
 
 void sim::set_boundary_conditions_uv() {
 
-    Kokkos::parallel_for("set_bc_u_y", yn_+1, KOKKOS_LAMBDA(int j) {
+    Kokkos::parallel_for("set_bc_u_y", Kokkos::RangePolicy<>(1, yn_+1), KOKKOS_LAMBDA(int j) {
         u_(0, j) = 1.0;
         u_(1, j) = 1.0;
         v_(0, j) = 1.0;
@@ -67,8 +50,8 @@ void sim::set_boundary_conditions_uv() {
         v_(xn_, j) = 2.0 * v_(xn_-1, j) - v_(xn_-2, j);
         v_(xn_ + 1, j) = 2.0 * v_(xn_, j) - v_(xn_-1, j);
     });
-    
-    Kokkos::parallel_for("set_bc_v_x", xn_+1, KOKKOS_LAMBDA(int i) {
+    Kokkos::fence();
+    Kokkos::parallel_for("set_bc_v_x", Kokkos::RangePolicy<>(1, xn_+1), KOKKOS_LAMBDA(int i) {
         v_(i, 1) = 2.0 * v_(i, 2) - v_(i, 3);
         v_(i, 0) = 2.0 * v_(i, 1) - v_(i, 2);
         u_(i, 1) = 2.0 * u_(i, 2) - u_(i, 3);
@@ -78,37 +61,38 @@ void sim::set_boundary_conditions_uv() {
         u_(i, yn_) = 2.0 * u_(i, yn_-1) - u_(i, yn_-2);
         u_(i, yn_ + 1) = 2.0 * u_(i, yn_) - u_(i, yn_-1);
     });
-
-    Kokkos::parallel_for("set_ic", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({i_1, j_1}, {i_2, j_2}), 
+    Kokkos::fence();
+    Kokkos::parallel_for("set_ic", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({i_1, j_1}, {i_2+1, j_2+1}), 
     KOKKOS_LAMBDA(int i, int j) {
         u_(i, j) = 0.0;
         v_(i, j) = 0.0;
     });
-
+    
     Kokkos::fence();
 }
 
 void sim::set_boundary_conditions_p() {
 
-    Kokkos::parallel_for("p_x_bc", {1, xn_}, KOKKOS_LAMBDA(int i) {
+    Kokkos::parallel_for("p_x_bc", Kokkos::RangePolicy<>(1, xn_+1), KOKKOS_LAMBDA(int i) {
         p_(i, 0) = 0.0;
         p_(i, yn_-1) = 0.0;
     });
-
-    Kokkos::parallel_for("p_y_bc", {1, yn_}, KOKKOS_LAMBDA(int j) {
+    Kokkos::fence();
+    Kokkos::parallel_for("p_y_bc", Kokkos::RangePolicy<>(1, yn_+1), KOKKOS_LAMBDA(int j) {
         p_(0, j) = 0.0;
         p_(xn_-1, j) = 0.0;
     });
-
+    Kokkos::fence();
     p_(i_1, j_1) = p_(i_1-1, j_1-1);
     p_(i_2, j_2) = p_(i_2+1, j_2+1);
     p_(i_1, j_2) = p_(i_1-1, j_2+1);
     p_(i_2, j_1) = p_(i_2+1, j_1-1);
-    Kokkos::parallel_for("p_bloc_bc", {i_1+1, i_2}, KOKKOS_LAMBDA(int i) {
+    Kokkos::parallel_for("p_bloc_bc", Kokkos::RangePolicy<>(i_1+1, i_2), KOKKOS_LAMBDA(int i) {
         p_(i, j_1) = p_(i, j_1-1);
         p_(i, j_2) = p_(i, j_2+1);
     });
-    Kokkos::parallel_for("p_bloc_bc", {j_1+1, j_2}, KOKKOS_LAMBDA(int j) {
+    Kokkos::fence();
+    Kokkos::parallel_for("p_bloc_bc", Kokkos::RangePolicy<>(j_1+1, j_2), KOKKOS_LAMBDA(int j) {
         p_(i_1, j) = p_(i_1-1, j);
         p_(i_2, j) = p_(i_2+1, j);
     });
@@ -119,32 +103,48 @@ void sim::set_boundary_conditions_p() {
 void sim::poisson_solver() {
     Kokkos::View<double**> rhs("rhs", xn_ + 2, yn_ + 2);
     Kokkos::deep_copy(rhs, 0.0);
-    Kokkos::parallel_for("ps_rhs", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_-1, yn_-1}), 
+    Kokkos::fence();
+    Kokkos::parallel_for("ps_rhs", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_, yn_}), 
     KOKKOS_LAMBDA(int i, int j) {
-        if (in_square(i, j)) continue;
+        if (!in_square(i, j)) {
 
-        double ux = u_(i+1, j) - u_(i-1, j) / (2.0 *dx_);
-        double uy = u_(i, j+1) - u_(i, j-1) / (2.0 * dy_);
-        double vx = v_(i+1, j) - v_(i-1, j) / (2.0 * dx_);
-        double vy = v_(i, j+1) - v_(i, j-1) / (2.0 * dy_);
-        rhs(i,j) = (ux + vy) / dt - (ux * ux + 2.0 * uy * vx + vy * vy);
-    });
+            double ux = (u_(i+1, j) - u_(i-1, j)) / (2.0 * dx_);
+            double uy = (u_(i, j+1) - u_(i, j-1)) / (2.0 * dy_);
+            double vx = (v_(i+1, j) - v_(i-1, j)) / (2.0 * dx_);
+            double vy = (v_(i, j+1) - v_(i, j-1)) / (2.0 * dy_);
+            rhs(i,j) = (ux + vy) / dt - (ux * ux + 2.0 * uy * vx + vy * vy);
 
-    for (int iter = 0; iter < maxitp; i++) {
-        double res = 0.0;
-        Kokkos::parallel_for("sor", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_-1, yn_-1}), 
-        KOKKOS_LAMBDA(int i, int j) {
-            if (in_square(i, j)) continue;
-
-            double dp = ((p_(i+1, j) + p_(i-1, j)) / (dx * dx) +
-                        (p_(i, j+1) + p_(i, j-1)) / (dy * dy) - rhs(i,j)) /
-                        (2.0 / (dx * dx) + 2.0 / (dy * dy)) - p(i,j);
-
-            res += dp * dp;
-            p_(i,j) += omegap * dp;
         }
+    });
+    Kokkos::fence();
+
+    for (int iter = 0; iter < maxitp; iter++) {
+        double res = 0.0;
+        Kokkos::parallel_for("sor", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_, yn_}), 
+        KOKKOS_LAMBDA(int i, int j) {
+            if (!in_square(i, j)) {
+
+                double dp = ((p_(i+1, j) + p_(i-1, j)) / (dx_ * dx_) +
+                            (p_(i, j+1) + p_(i, j-1)) / (dy_ * dy_) - rhs(i,j)) /
+                            (2.0 / (dx_ * dx_) + 2.0 / (dy_ * dy_)) - p_(i,j);
+                p_(i,j) += omegap * dp;
+            }
+        });
+        Kokkos::fence();
+        Kokkos::parallel_reduce("sor", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_, yn_}), 
+        KOKKOS_LAMBDA(int i, int j, double& local_res) {
+            if (!in_square(i, j)) {
+
+                double dp = ((p_(i+1, j) + p_(i-1, j)) / (dx_ * dx_) +
+                            (p_(i, j+1) + p_(i, j-1)) / (dy_ * dy_) - rhs(i,j)) /
+                            (2.0 / (dx_ * dx_) + 2.0 / (dy_ * dy_)) - p_(i,j);
+
+                local_res += dp * dp;
+            }
+        }, res);
+        Kokkos::fence();
         sim::set_boundary_conditions_p();
-        res = sqrt(res / double(xn_, xy_)));
+        res = sqrt(res / double(xn_ * yn_));
         if (res < errorp) break;
     }
 
@@ -153,33 +153,39 @@ void sim::poisson_solver() {
 
 void sim::velocity() {
     Kokkos::View<double**> urhs("rhs", xn_ + 2, yn_ + 2);
-    Kokkos::deep_copy(rhs, 0.0);
+    Kokkos::deep_copy(urhs, 0.0);
     Kokkos::View<double**> vrhs("rhs", xn_ + 2, yn_ + 2);
-    Kokkos::deep_copy(rhs, 0.0);
+    Kokkos::deep_copy(vrhs, 0.0);
 
-    Kokkos::parallel_for("set_vel_rhs", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1,0}, {xn_, yn_}), 
+    Kokkos::fence();
+
+    Kokkos::parallel_for("set_vel_rhs", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_, yn_}), 
     KOKKOS_LAMBDA(int i, int j) {
-        if (in_square(i, j)) continue;
+        if (!in_square(i, j)) {
 
-        urhs(i,j) = -(p_(i+1,j) - p_(i-1,j)) / (2.0 * dx_) + 
-                    (u_(i+1,j) - 2.0 *u_(i,j) + u_(i-1,j)) / (Re * dx * dx) +
-                    (u_(i,j+1) - 2.0 *u_(i,j) + u_(i,j-1)) / (Re * dy * dy) -
-                    KKSchemeX(u_(i,j), u_, i, j) - KKSchemeY(u_(i,j), u_, i, j);
+            urhs(i,j) = -(p_(i+1,j) - p_(i-1,j)) / (2.0 * dx_) + 
+                        (u_(i+1,j) - 2.0 *u_(i,j) + u_(i-1,j)) / (Re * dx_ * dx_) +
+                        (u_(i,j+1) - 2.0 *u_(i,j) + u_(i,j-1)) / (Re * dy_ * dy_) -
+                        KKSchemeX(u_(i,j), u_, i, j) - KKSchemeY(u_(i,j), u_, i, j);
 
-        vrhs(i,j) = -(p_(i+1,j) - p_(i-1,j)) / (2.0 * dx_) + 
-                    (v_(i+1,j) - 2.0 *v_(i,j) + v_(i-1,j)) / (Re * dx * dx) +
-                    (v_(i,j+1) - 2.0 *v_(i,j) + v_(i,j-1)) / (Re * dy * dy) -
-                    KKSchemeX(v_(i,j), v_, i, j) - KKSchemeY(v_(i,j), v_, i, j);
+            vrhs(i,j) = -(p_(i+1,j) - p_(i-1,j)) / (2.0 * dx_) + 
+                        (v_(i+1,j) - 2.0 *v_(i,j) + v_(i-1,j)) / (Re * dx_ * dx_) +
+                        (v_(i,j+1) - 2.0 *v_(i,j) + v_(i,j-1)) / (Re * dy_ * dy_) -
+                        KKSchemeX(v_(i,j), v_, i, j) - KKSchemeY(v_(i,j), v_, i, j);
 
-
+        }
     });
 
-    Kokkos::parallel_for("set_ic", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({1,0}, {xn_, yn_}), 
-    KOKKOS_LAMBDA(int i, int j) {
-        if (in_square(i, j)) continue;
+    Kokkos::fence();
 
-        u_(i,j) = u_(i,j) + dt * urhs(i,j);
-        v_(i,j) = v_(i,j) + dt * vrhs(i,j);
+    Kokkos::parallel_for("set_ic", Kokkos::MDRangePolicy<Kokkos::Rank<2>>({2,2}, {xn_, yn_}), 
+    KOKKOS_LAMBDA(int i, int j) {
+        if (!in_square(i, j)) {
+
+            u_(i,j) = u_(i,j) + dt * urhs(i,j);
+            v_(i,j) = v_(i,j) + dt * vrhs(i,j);
+
+        }
     });
 
     Kokkos::fence();
@@ -196,24 +202,24 @@ void sim::save_vtk(const std::string& filename) {
     vtk_file << "DATASET STRUCTURED_GRID\n";
     
     // Grid dimensions (2D)
-    vtk_file << "DIMENSIONS " << xn_+1 << " " << yn_+1 << " 1\n";
+    vtk_file << "DIMENSIONS " << xn_+2 << " " << yn_+2 << " 1\n";
     
     // Coordinates (2D plane)
-    vtk_file << "POINTS " << (xn_+1)*(yn_+1) << " float\n";
-    for (int j = 0; j <= yn_; ++j) {
-        for (int i = 0; i <= xn_; ++i) {
+    vtk_file << "POINTS " << (xn_+2)*(yn_+2) << " float\n";
+    for (int j = 0; j < yn_+2; ++j) {
+        for (int i = 0; i < xn_+2; ++i) {
             vtk_file << i*dx_ << " " << j*dy_ << " 0.0\n";
         }
     }
     
     // Velocity data (staggered to grid points)
-    vtk_file << "POINT_DATA " << (xn_+1)*(yn_+1) << "\n";
+    vtk_file << "POINT_DATA " << (xn_+2)*(yn_+2) << "\n";
     vtk_file << "VECTORS velocity float\n";
-    for (int j = 0; j <= yn_; ++j) {
-        for (int i = 0; i <= xn_; ++i) {
+    for (int j = 0; j < yn_+2; ++j) {
+        for (int i = 0; i < xn_+2; ++i) {
             // Interpolate u,v to grid points (simple average)
-            float u = (i > 0) ? 0.5*(u_(i-1,j) + u_(i,j)) : u_(i,j);
-            float v = (j > 0) ? 0.5*(v_(i,j-1) + v_(i,j)) : v_(i,j);
+            float u = (i > 0) ? 0.5f*(u_(i-1,j) + u_(i,j)) : u_(i,j);
+            float v = (j > 0) ? 0.5f*(v_(i,j-1) + v_(i,j)) : v_(i,j);
             vtk_file << u << " " << v << " 0.0\n";
         }
     }
@@ -221,19 +227,32 @@ void sim::save_vtk(const std::string& filename) {
     // Pressure data (cell-centered → interpolated to nodes)
     vtk_file << "SCALARS pressure float 1\n";
     vtk_file << "LOOKUP_TABLE default\n";
-    for (int j = 0; j <= yn_; ++j) {
-        for (int i = 0; i <= xn_; ++i) {
+    for (int j = 0; j < yn_+2; ++j) {
+        for (int i = 0; i < xn_+2; ++i) {
             // Average pressure from adjacent cells
-            float p_val = 0.0;
+            float p_val = 0.0f;
             int count = 0;
             if (i > 0 && j > 0) { p_val += p_(i-1,j-1); count++; }
-            if (i < xn_ && j > 0) { p_val += p_(i,j-1); count++; }
-            if (i > 0 && j < yn_) { p_val += p_(i-1,j); count++; }
-            if (i < xn_ && j < yn_) { p_val += p_(i,j); count++; }
-            vtk_file << (count > 0 ? p_val/count : 0.0) << "\n";
+            if (i < xn_+1 && j > 0) { p_val += p_(i,j-1); count++; }
+            if (i > 0 && j < yn_+1) { p_val += p_(i-1,j); count++; }
+            if (i < xn_+1 && j < yn_+1) { p_val += p_(i,j); count++; }
+            vtk_file << (count > 0 ? p_val/count : 0.0f) << "\n";
         }
     }
     
     vtk_file.close();
 }
+
+void sim::save_pressure_matrix_csv(const std::string& filename) {
+    std::ofstream ofs(filename);
+    if (!ofs) {
+        std::cerr << "Could not open file for writing: " << filename << std::endl;
+        exit(-1);
+    }
+    for (int i = yn_; i >= 1; i--) {
+        for (int j = 1; j <= xn_; j++) {
+            ofs << p_(i, j);
+            ofs << ((j == xn_) ? "\n" : ",");
+        }
+    }
 }
